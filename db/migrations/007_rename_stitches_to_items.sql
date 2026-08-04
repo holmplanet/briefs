16fac1384916a70a61786714250fcf8fb519bf1c
holmplanet <holmplanet@gmail.com>
2026-08-04 17:08:48 -0400

    chore(db): squash migrations into single initial schema
    Replace incremental stitch/item migration history with 001_initial.sql
    since the database was never deployed.

--- db/migrations/007_rename_stitches_to_items.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE stitch_nodes RENAME TO items;
-
-ALTER INDEX IF EXISTS idx_stitch_nodes_user RENAME TO idx_items_user;
-ALTER INDEX IF EXISTS idx_stitch_nodes_user_status RENAME TO idx_items_user_status;
-ALTER INDEX IF EXISTS idx_stitch_nodes_context RENAME TO idx_items_context;
-ALTER INDEX IF EXISTS idx_stitch_nodes_archive_status RENAME TO idx_items_archive_status;
-
-UPDATE items SET object_type = 'item' WHERE object_type = 'stitch';
-ALTER TABLE items ALTER COLUMN object_type SET DEFAULT 'item';
